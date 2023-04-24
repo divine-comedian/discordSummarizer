2d3bce0cb90eb20d479a48ff08872ecb95e6a067
Mitch Oz <divine_comedian@protonmail.com>
2023-04-24 15:07:12 -0600

    first commit

---
+++ bot.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+const axios = require('axios');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.MESSAGE_CONTENT, Intents.FLAGS.GUILD_MESSAGES
+] });
+
+const OPENAI_API_BASE_URL = "https://api.openai.com/v1/chat/completions"; // Update this line
+const OPENAI_API_KEY= process.env.OPENAI_GPT3_5_API
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content === '!aisummarize20' && !message.author.bot) {
+    try {
+      const messages = await message.channel.messages.fetch({ limit: 20 });
+      const messageContentArray = messages.map((msg) => `${msg.author.username}: ${msg.content}`);
+      const messageContent = messageContentArray.join('\n');
+      const summary = await fetchSummary(messageContent);
+      await summary;
+      console.log("this is the summary", summary)
+      message.channel.send(`Here's the summary:\n${summary}`);
+    } catch (error) {
+      console.error(error);
+      message.channel.send('An error occurred while fetching the summary.');
+    }
+  }
+});
+
+async function fetchSummary(text) {
+    const axiosInstance = axios.create({
+        baseURL: OPENAI_API_BASE_URL,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        },
+      });
+
+  const response = await axiosInstance.post(OPENAI_API_BASE_URL, 
+    {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {"role": "system", "content": "Your goal is to provide summaries and insights for users of Giveth to quickly catch up on a conversation in a specific discord channel."},
+          {"role": "user", "content": `Please summarize the following conversation:\n${text}`}
+        ],
+      }
+    
+    // JSON.stringify({
+    //   prompt: `Please summarize the following conversation:\n${text}`,
+    //   max_tokens: 100,
+    //   n: 1,
+    //   stop: null,
+    //   temperature: 0.5,
+    // }),
+  );
+
+  return response.data.choices[0].message.content;
+}
+
+client.on('ready', () => {
+    console.log(`Ready for action! Logged in as ${client.user.tag}.`);
+  });
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+
